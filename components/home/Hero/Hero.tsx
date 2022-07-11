4fa5ebd4e749969d87e6873b7621ed56ecac42ff
Shadman Shakib <sm.shakib007@gmail.com>
2022-07-11 21:59:20 +0600

    Swiper

---
+++ components/home/Hero/Hero.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+function Hero() {
+  return (
+    <div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+
+export default Hero;
