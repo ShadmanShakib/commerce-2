1944cf87c2c546a53223e094a12dbbde12e5fb1c
Shadman Shakib <sm.shakib007@gmail.com>
2022-08-09 11:22:23 +0600

    Prodinfo

--- components/product/ProdInfo/ProdInfo.tsx
+++ components/product/ProdInfo/ProdInfo.tsx
@@ -1,23 +1,39 @@
 import React from "react";
 import { Button } from "components/ui";
+import { BiShare } from "react-icons/bi";
 export default function ProdInfo() {
   return (
     <article className="w-1/2 px-8">
-      <h1 className="text-2xl">White Ceramic Flower Mug</h1>
-      <div className="flex">
-        <p className="text-xl">$22.99</p>
-        <p>$28.50</p>
+      <h1 className="mb-5 text-2xl">White Ceramic Flower Mug</h1>
+      <div className="flex items-center">
+        <h1 className="mr-4 text-xl">$22.99</h1>
+        <p className="line-through">$28.50</p>
       </div>
       <h1>Quantity</h1>
-      <div className="flex">
-        <Button className="w-full" varient="outline">
+      <div className="mb-6 flex">
+        <div className="grid grid-cols-3 content-center ">
+          <Button>+</Button>
+          <input defaultValue={1} type="number" />
+          <Button>-</Button>
+        </div>
+        <Button className="mb-6 w-full" varient="outline">
           <span>Add to cart</span>
         </Button>
       </div>
       <div>
-        <Button varient="primary">
+        <Button className="w-full" varient="dark">
           <span>Buy it now</span>
         </Button>
+      </div>
+      <div className="flex ">
+        <div className="mr-5">
+          <span>Compare</span>
+        </div>
+        <div className="mr-5">Ask a question</div>
+        <div className="flex items-center">
+          <BiShare size={20} />
+          Share
+        </div>
       </div>
     </article>
   );
--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -3,13 +3,16 @@ import { ButtonProps } from "./Button.types";
 import cn from "classnames";
 function Button(props: ButtonProps) {
   const className = cn(
-    " text-black font-semibold  hover:scale-105 transform hover:bg-black hover:text-white transform duration-300",
+    "  font-semibold  hover:scale-105 transform   transform duration-300",
     props.className,
     {
-      "rounded-md px-5 py-2 bg-white ": props.varient === "primary",
-      "rounded-full p-3 bg-white": props.varient === "rounded",
-      "px-5 py-2 bg-transparent rounded-md border-2 border-black":
+      "rounded-md px-5 py-2": props.varient !== "rounded",
+      " bg-white hover:bg-black hover:text-white text-black ":
+        props.varient === "primary",
+      "rounded-full p-3  ": props.varient === "rounded",
+      "text-black bg-transparent  border-2 border-black bg-white hover:bg-black hover:text-white text-black  ":
         props.varient === "outline",
+      "bg-black text-white": props.varient === "dark",
     }
   );
   return <button className={className}>{props.children}</button>;
