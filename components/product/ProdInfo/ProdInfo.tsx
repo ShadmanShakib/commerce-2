57306133d36e46b06b683d24323ed193f1cdd7cb
Shadman Shakib <sm.shakib007@gmail.com>
2022-08-11 19:36:31 +0600

    ProdInfo

--- components/product/ProdInfo/ProdInfo.tsx
+++ components/product/ProdInfo/ProdInfo.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { Button } from "components/ui";
-import { BiShare } from "react-icons/bi";
+import { BiShare, BiQuestionMark, } from "react-icons/bi";
 export default function ProdInfo() {
   return (
-    <article className="w-1/2 px-8 flex flex-col items-center">
+    //main
+    <section className="w-1/2 px-8 flex flex-col justify-center ">
       <h1 className="mb-5 text-2xl">White Ceramic Flower Mug</h1>
       <div className="flex items-center">
         <h1 className="mr-4 text-xl">$22.99</h1>
         <p className="line-through">$28.50</p>
       </div>
       <h1>Quantity</h1>
+      {/* quantity */}
       <div className="mb-6 flex">
-        <div className="grid grid-cols-3 content-center ">
-          <Button>+</Button>
-          <input defaultValue={1} type="number" />
-          <Button>-</Button>
+        <div className="flex items-center ">
+          <Button className="w-6">+</Button>
+          <input className="w-6 outline-none" defaultValue={1} type="number" />
+          <Button className="w-6">-</Button>
         </div>
         <Button className="mb-6 w-full" varient="outline">
           <span>Add to cart</span>
@@ -25,16 +27,21 @@ export default function ProdInfo() {
           <span>Buy it now</span>
         </Button>
       </div>
-      <div className="flex ">
-        <div className="mr-5">
+      <div className="flex mt-6">
+        <div className="mr-5 flex items-center">
+          <BiQuestionMark size={20} />
           <span>Compare</span>
         </div>
-        <div className="mr-5">Ask a question</div>
+        <div className="mr-5 flex items-center">
+          <BiQuestionMark size={20} />
+          <span>   Ask a question</span>
+
+        </div>
         <div className="flex items-center">
           <BiShare size={20} />
           Share
         </div>
       </div>
-    </article>
+    </section>
   );
 }
