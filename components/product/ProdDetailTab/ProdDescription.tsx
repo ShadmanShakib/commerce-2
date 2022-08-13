5b1403f0bd979a495ed64ffde14841f3735465f7
Shadman Shakib <sm.shakib007@gmail.com>
2022-08-13 12:38:41 +0600

    Product description

--- components/product/ProdDetailTab/ProdDescription.tsx
+++ components/product/ProdDetailTab/ProdDescription.tsx
@@ -5,7 +5,7 @@ export default function ProdDescription(props: ProdDescriptionProps) {
   const { description, src, title } = props;
   return (
     <article className=" flex w-full py-10">
-      <div className="relative w-96">
+      <div className="w-3/5">
         <Image
           layout="responsive"
           height={100}
@@ -14,8 +14,8 @@ export default function ProdDescription(props: ProdDescriptionProps) {
           alt={title}
         />
       </div>
-      <div>
-        <h1>The Iconic Silhouette</h1>
+      <div className="ml-6">
+        <h1 className="mb-6 text-xl font-semibold">The Iconic Silhouette</h1>
         <p>{description}</p>
       </div>
     </article>
