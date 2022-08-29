fb34cadbd3b5d2ad9324132a9f1d1eea162b0390
Shadman Shakib <sm.shakib007@gmail.com>
2022-08-29 06:36:00 +0600

    id added

--- components/product/Recommendation/Recommendation.tsx
+++ components/product/Recommendation/Recommendation.tsx
@@ -5,6 +5,7 @@ function Recommendation() {
     <section className="mx-auto max-w-screen-xl">
       <h1 className="mb-6 text-4xl">You Might Also Like</h1>
       <ProductCard
+        id="shfh"
         title="Blue Floral Face Mask"
         image="https://cdn.shopify.com/s/files/1/0147/0962/0800/products/20_a_800x.png?v=1624267241
 "
